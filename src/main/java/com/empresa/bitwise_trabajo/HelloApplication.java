80ccc0e1666744f8a5935ff7c72569230ff94bd7
Juanjo <juanjose.acebedolara.23@campusfp.es>
2024-05-19 01:33:50 +0200

    estilamos la aplicacion y añadimos el control de excepciones

--- src/main/java/com/empresa/bitwise_trabajo/HelloApplication.java
+++ src/main/java/com/empresa/bitwise_trabajo/HelloApplication.java
@@ -10,15 +10,16 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/empresa/bitwise_trabajo/hello-view.fxml"));
         Scene scene = null;
         try {
-            scene = new Scene(fxmlLoader.load(), 800, 600);
+            scene = new Scene(fxmlLoader.load(), 900, 750);
+            scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
         }
         catch (IOException e) {
             e.printStackTrace();
         }
-        stage.setTitle("Hello!");
+        stage.setTitle("OPERADORES BITWISE!");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/empresa/bitwise_trabajo/HelloController.java
+++ src/main/java/com/empresa/bitwise_trabajo/HelloController.java
@@ -11,52 +11,127 @@ public class HelloController {
     private TextField number2;
     @FXML
     private Label resultText;
+    @FXML
+    private Label binaryNumber1;
+    @FXML
+    private Label binaryNumber2;
+    @FXML
+    private Label binaryResultText;
+
+    @FXML
+    protected void onNumber1KeyReleased() {
+        try {
+            int num1 = Integer.parseInt(number1.getText());
+            binaryNumber1.setText("Binary: " + Integer.toBinaryString(num1));
+        } catch (NumberFormatException e) {
+            binaryNumber1.setText("Invalid number");
+        }
+    }
+
+    @FXML
+    protected void onNumber2KeyReleased() {
+        try {
+            int num2 = Integer.parseInt(number2.getText());
+            binaryNumber2.setText("Binary: " + Integer.toBinaryString(num2));
+        } catch (NumberFormatException e) {
+            binaryNumber2.setText("Invalid number");
+        }
+    }
 
     @FXML
     protected void onAndButtonClick() {
-        int num1 = Integer.parseInt(number1.getText());
-        int num2 = Integer.parseInt(number2.getText());
-        resultText.setText("Result: " + (num1 & num2));
+        try {
+            int num1 = Integer.parseInt(number1.getText());
+            int num2 = Integer.parseInt(number2.getText());
+            int result = num1 & num2;
+            resultText.setText("Result: " + result);
+            binaryResultText.setText("Binary: " + Integer.toBinaryString(result));
+        } catch (NumberFormatException e) {
+            resultText.setText("Please enter valid numbers");
+            binaryResultText.setText("");
+        }
     }
 
     @FXML
     protected void onOrButtonClick() {
-        int num1 = Integer.parseInt(number1.getText());
-        int num2 = Integer.parseInt(number2.getText());
-        resultText.setText("Result: " + (num1 | num2));
+        try {
+            int num1 = Integer.parseInt(number1.getText());
+            int num2 = Integer.parseInt(number2.getText());
+            int result = num1 | num2;
+            resultText.setText("Result: " + result);
+            binaryResultText.setText("Binary: " + Integer.toBinaryString(result));
+        } catch (NumberFormatException e) {
+            resultText.setText("Please enter valid numbers");
+            binaryResultText.setText("");
+        }
     }
 
     @FXML
     protected void onXorButtonClick() {
-        int num1 = Integer.parseInt(number1.getText());
-        int num2 = Integer.parseInt(number2.getText());
-        resultText.setText("Result: " + (num1 ^ num2));
+        try {
+            int num1 = Integer.parseInt(number1.getText());
+            int num2 = Integer.parseInt(number2.getText());
+            int result = num1 ^ num2;
+            resultText.setText("Result: " + result);
+            binaryResultText.setText("Binary: " + Integer.toBinaryString(result));
+        } catch (NumberFormatException e) {
+            resultText.setText("Please enter valid numbers");
+            binaryResultText.setText("");
+        }
     }
 
     @FXML
     protected void onNotButtonClick() {
-        int num1 = Integer.parseInt(number1.getText());
-        resultText.setText("Result: " + (~num1));
+        try {
+            int num1 = Integer.parseInt(number1.getText());
+            int result = ~num1;
+            resultText.setText("Result: " + result);
+            binaryResultText.setText("Binary: " + Integer.toBinaryString(result));
+        } catch (NumberFormatException e) {
+            resultText.setText("Please enter a valid number");
+            binaryResultText.setText("");
+        }
     }
 
     @FXML
     protected void onLeftShiftButtonClick() {
-        int num1 = Integer.parseInt(number1.getText());
-        int num2 = Integer.parseInt(number2.getText());
-        resultText.setText("Result: " + (num1 << num2));
+        try {
+            int num1 = Integer.parseInt(number1.getText());
+            int num2 = Integer.parseInt(number2.getText());
+            int result = num1 << num2;
+            resultText.setText("Result: " + result);
+            binaryResultText.setText("Binary: " + Integer.toBinaryString(result));
+        } catch (NumberFormatException e) {
+            resultText.setText("Please enter valid numbers");
+            binaryResultText.setText("");
+        }
     }
 
     @FXML
     protected void onRightShiftButtonClick() {
-        int num1 = Integer.parseInt(number1.getText());
-        int num2 = Integer.parseInt(number2.getText());
-        resultText.setText("Result: " + (num1 >> num2));
+        try {
+            int num1 = Integer.parseInt(number1.getText());
+            int num2 = Integer.parseInt(number2.getText());
+            int result = num1 >> num2;
+            resultText.setText("Result: " + result);
+            binaryResultText.setText("Binary: " + Integer.toBinaryString(result));
+        } catch (NumberFormatException e) {
+            resultText.setText("Please enter valid numbers");
+            binaryResultText.setText("");
+        }
     }
 
     @FXML
     protected void onUnsignedRightShiftButtonClick() {
-        int num1 = Integer.parseInt(number1.getText());
-        int num2 = Integer.parseInt(number2.getText());
-        resultText.setText("Result: " + (num1 >>> num2));
+        try {
+            int num1 = Integer.parseInt(number1.getText());
+            int num2 = Integer.parseInt(number2.getText());
+            int result = num1 >>> num2;
+            resultText.setText("Result: " + result);
+            binaryResultText.setText("Binary: " + Integer.toBinaryString(result));
+        } catch (NumberFormatException e) {
+            resultText.setText("Please enter valid numbers");
+            binaryResultText.setText("");
+        }
     }
 }
